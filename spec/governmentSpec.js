ea51f7c60e2dedf473a4537a41136e4c47788e31
anthonygarvan <anthony.garvan@gmail.com>
2015-08-09 08:12:13 -0500

    adding govt isalive test

---
+++ spec/governmentSpec.js
@@ -0,0 +1,9 @@
+var request = require("request")
+describe("Make sure government is alive", function() {
+  it("is alive", function(done) {
+      request.get("http://localhost:3000/government/isalive", function(err, response, body) {
+            expect(!err && response.statusCode == 200).toBe(true);
+            done();
+      })
+  });
+});
--- spec/serverSpec.js
+++ spec/serverSpec.js
@@ -1,5 +1,5 @@
 var request = require("request")
-describe("A suite", function() {
+describe("Make sure server is running", function() {
   it("can serve from the root directory", function(done) {
       request.get("http://localhost:3000/", function(err, response, body) {
             expect(!err && response.statusCode == 200).toBe(true);
