1e95a719163ad8b680ecd3da4a940ab3b4caf003
anthonygarvan <anthony.garvan@gmail.com>
2015-08-21 10:42:34 -0500

    adding skeleton demo app

---
+++ public/js/republic-demo.js
@@ -0,0 +1,5 @@
+angular.module('app', [])
+  .controller('citizenshipController', function() {
+    //var citizenship = this;
+    this.submitApplication = function() {alert("submitted!");}
+  });
--- server.js
+++ server.js
@@ -6,10 +6,9 @@ var spawn = require('child_process').spawn;
 app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.get('/', function (req, res) {
-  res.send('Welcome to Republic\'s Home Page!');
-});
+
 app.use('/government', government.router);
+app.use(express.static('public'));
 
 exports.start = function( config, readyCallback ) {
     if(!this.server) {
